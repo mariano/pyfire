4039a404c1e1a623d1210054b16eae7071c95d27
Mariano Iglesias <mariano@cricava.com>
2010-08-19 23:17:54 -0300

    Adding implementation of MultiPartProducer and StringReceiver for twisted

---
+++ pyfire/twistedx/receiver.py
@@ -0,0 +1,18 @@
+from twisted.internet import protocol
+from twisted.web import client
+from twisted.internet import reactor
+
+class StringReceiver(protocol.Protocol):
+	buffer = ""
+
+	def __init__(self, deferred=None):
+		self._deferred = deferred
+
+	def dataReceived(self, data):
+		self.buffer += data
+
+	def connectionLost(self, reason):
+		if self._deferred and reason.check(client.ResponseDone):
+			self._deferred.callback(self.buffer)
+		else:
+			self._deferred.errback(self.buffer)
